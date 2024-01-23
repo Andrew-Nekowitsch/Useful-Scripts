40151736972c7157c65adc32106e439c7f9815f6
Andrew Nekowitsch <anekowitsch@gmail.com>
2024-01-22 23:39:47 -0600

    Update TitleCaseFoldersInDirectory.py

--- TitleCaseFoldersInDirectory.py
+++ TitleCaseFoldersInDirectory.py
@@ -6,16 +6,18 @@ def title_case_folders(directory_path):
 
     # Iterate through each folder and rename to title case
     for folder in folders:
-        old_path = os.path.join(directory_path, folder)
         new_folder = folder.title()
-        new_path = os.path.join(directory_path, new_folder)
+        if folder != new_folder:
+            old_path = os.path.join(directory_path, folder)
+            new_path = os.path.join(directory_path, new_folder)
 
-        # Rename the folder
-        os.rename(old_path, new_path)
-        print(f'Renamed: {folder} to {new_folder}')
+            # Rename the folder
+            os.rename(old_path, new_path)
+            print(f'Renamed: {folder} to {new_folder}')
+        print(f'Skipped: {folder}')
 
 if __name__ == "__main__":
     # Replace 'your_directory_path' with the actual path of the directory you want to process
-    directory_path = 'Z:\\tv'
+    directory_path = 'Z:\\movies'
     
     title_case_folders(directory_path)
